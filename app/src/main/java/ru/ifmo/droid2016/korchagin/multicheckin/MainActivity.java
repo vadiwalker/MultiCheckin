3ebac0501b4d74b2cef1d07c398e684d3f016f1e
zclimber <mywork-olymps@yandex.ru>
2016-12-17 16:40:53 +0300

    Implemented basic image selecting and undo

--- app/src/main/java/ru/ifmo/droid2016/korchagin/multicheckin/MainActivity.java
+++ app/src/main/java/ru/ifmo/droid2016/korchagin/multicheckin/MainActivity.java
@@ -1,31 +1,77 @@
 package ru.ifmo.droid2016.korchagin.multicheckin;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View[] step1, step2;
+
+    private ImageView imageView;
+
+    private static final int REQUEST_PICTURE_CAPTURE = 1;
+    private static final int REQUEST_PICTURE_FROM_FILE = 2;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK){
+            return;
+        }
+        switch (requestCode) {
+            case REQUEST_PICTURE_CAPTURE :
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                if(imageBitmap != null){
+                    gotoStep2(imageBitmap);
+                }
+                break;
+            case REQUEST_PICTURE_FROM_FILE :
+                Uri fileUri = data.getData();
+                try{
+                    imageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), fileUri);
+                    gotoStep2(imageBitmap);
+                } catch (IOException e){
+                    // silent
+                }
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)  findViewById(R.id.step2_image);
+
+        step1 = new View[4];
+        step1[0] = findViewById(R.id.step1_hint);
+        step1[1] = findViewById(R.id.step1_textOr);
+        step1[2] = findViewById(R.id.select_camera);
+        step1[3] = findViewById(R.id.select_file);
+
+        step2 = new View[3];
+        step2[0] = imageView;
+        step2[1] = findViewById(R.id.step2_undo);
+        step2[2] = findViewById(R.id.fab);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -33,6 +79,51 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void selectPhotoFromFile(View v){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            Toast errorToast = Toast.makeText(getBaseContext(), R.string.step1_nofile, Toast.LENGTH_SHORT);
+            errorToast.show();
+        }
+        startActivityForResult(intent, REQUEST_PICTURE_FROM_FILE);
+    }
+
+    public void selectPhotoFromCamera(View v){
+        if(!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            Toast errorToast = Toast.makeText(getBaseContext(), R.string.step1_nocamera, Toast.LENGTH_SHORT);
+            errorToast.show();
+            return;
+        }
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) == null) {
+            Toast errorToast = Toast.makeText(getBaseContext(), R.string.step1_nocamera, Toast.LENGTH_SHORT);
+            errorToast.show();
+            return;
+        }
+        startActivityForResult(takePictureIntent, REQUEST_PICTURE_CAPTURE);
+    }
+
+    public void gotoStep2(@NonNull Bitmap image){
+        imageView.setImageBitmap(image);
+        for (View view: step1) {
+            view.setVisibility(View.INVISIBLE);
+        }
+        for (View view: step2) {
+            view.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void undoStep2(View v){
+        for (View view: step1) {
+            view.setVisibility(View.VISIBLE);
+        }
+        for (View view: step2) {
+            view.setVisibility(View.INVISIBLE);
+        }
+        imageView.setImageDrawable(null);
     }
 
     @Override
