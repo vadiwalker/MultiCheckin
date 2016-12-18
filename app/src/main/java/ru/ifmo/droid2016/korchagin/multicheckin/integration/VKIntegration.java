af43f913cab515a1aeb09fcf055a8e843cfd001f
zclimber <mywork-olymps@yandex.ru>
2016-12-19 02:01:18 +0300

    Inserted "post to wall" example from VK API, as it does exactly the task

--- app/src/main/java/ru/ifmo/droid2016/korchagin/multicheckin/integration/VKIntegration.java
+++ app/src/main/java/ru/ifmo/droid2016/korchagin/multicheckin/integration/VKIntegration.java
@@ -5,18 +5,31 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiPhoto;
+import com.vk.sdk.api.model.VKAttachments;
+import com.vk.sdk.api.model.VKPhotoArray;
 import com.vk.sdk.api.model.VKScopes;
+import com.vk.sdk.api.model.VKWallPostResult;
+import com.vk.sdk.api.photo.VKImageParameters;
+import com.vk.sdk.api.photo.VKUploadImage;
 
 import java.lang.ref.WeakReference;
 
 import ru.ifmo.droid2016.korchagin.multicheckin.R;
+
 
 /**
  * Created by ME on 17.12.2016.
@@ -106,5 +119,50 @@ public class VKIntegration implements SocialIntegration{
     @Override
     public boolean getStatus() {
         return VKSdk.isLoggedIn();
+    }
+
+    private int getMyVKId() {
+        final VKAccessToken vkAccessToken = VKAccessToken.currentToken();
+        return vkAccessToken != null ? Integer.parseInt(vkAccessToken.userId) : 0;
+    }
+
+    /**
+     * Отправляет пару из фоточки и коммента на стенку через AsyncTask
+     * @param photo Bitmap фотки
+     * @param comment Комментарий
+     */
+    public boolean sendPhotos(@NonNull Bitmap photo, @Nullable final String comment){
+        VKRequest request = VKApi.uploadWallPhotoRequest(new VKUploadImage(photo,
+            VKImageParameters.jpgImage(0.9f)), getMyVKId(), 0);
+        request.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                VKApiPhoto photoModel = ((VKPhotoArray) response.parsedModel).get(0);
+                makePost(new VKAttachments(photoModel), comment, getMyVKId());
+            }
+            @Override
+            public void onError(VKError error) {
+            }
+        });
+        return false;
+    }
+
+    void makePost(VKAttachments att, String msg, final int ownerId) {
+        VKParameters parameters = new VKParameters();
+        parameters.put(VKApiConst.OWNER_ID, String.valueOf(ownerId));
+        parameters.put(VKApiConst.ATTACHMENTS, att);
+        parameters.put(VKApiConst.MESSAGE, msg);
+        VKRequest post = VKApi.wall().post(parameters);
+        post.setModelClass(VKWallPostResult.class);
+        post.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                // post was added
+            }
+            @Override
+            public void onError(VKError error) {
+                // error
+            }
+        });
     }
 }
